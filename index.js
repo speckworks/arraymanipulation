5055c7eaeb97a61d8c6c9d7709b6bb734beedc77
John K. Speck <speckworks@gmail.com>
2020-06-16 20:12:41 +0000

    solution

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 function arrayManipulation(n, queries) {
-    let a, b, k;
-        let array = [];
-    for(let i = 0; i < n; i++){
-    for(let j = a; j < k; i ++){
-        array[i] += k;
-        }
-    }
-    return array;
+let diffs = [];
+
+        queries.forEach((value, index) => {
+
+                const range_start = value[0];
+                const range_end = value[1];
+                const addend = value[2];
+
+                if (!diffs[range_start]) {
+                        diffs[range_start] = addend;
+                } else {
+                        diffs[range_start] += addend; 
+                }
+
+                if (!diffs[range_end + 1]) {
+                        diffs[range_end + 1] = 0 - addend;
+                } else {
+                        diffs[range_end + 1] -= addend;
+                }
+        });
+
+        return diffs.reduce((acc, cur) => {
+                return {
+                        running_total: acc.running_total + cur,
+                        max: Math.max(acc.max, acc.running_total + cur)
+                };
+        }, {running_total: 0, max: 0}).max;
 }
 
-arrayManipulation(10, [1 , 3 ,10])
