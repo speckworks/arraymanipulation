9e0dc006a30b0134b864839578b7bec82076febe
John K. Speck <speckworks@gmail.com>
2020-06-16 19:56:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function arrayManipulation(n, queries) {
+    let a, b, k;
+        let array = [];
+    for(let i = 0; i < n; i++){
+    for(let j = a; j < k; i ++){
+        array[i] += k;
+        }
+    }
+    return array;
+}
+
+arrayManipulation(10, [1 , 3 ,10])
